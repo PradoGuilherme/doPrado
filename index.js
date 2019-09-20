601a77eac0929011d43a5dbebefbecc121b9b069
Guilherme Prado <guilherme.oliveira@objective.com.br>
2019-09-20 15:44:08 -0300

    :tada:

---
+++ index.js
@@ -0,0 +1,25 @@
+console.log(JSON.stringify({
+  name: 'Guilherme Prado',
+  github: 'https://github.com/PradoGuilherme',
+  linkedin: 'https://www.linkedin.com/in/guinprado',
+  facebook: 'https://www.facebook.com/GuinPrado',
+  telegram: 'https://t.me/guinprado',
+  email: 'guilherme@devprado.com.br',
+  jobs: [
+    {
+      companyName: 'Byte (a) Byte',
+      initialDate: 'February, 2016',
+      finalDate: 'February, 2017'
+    },
+    {
+      companyName: 'TecnoSpeed',
+      initialDate: 'February, 2016',
+      finalDate: 'August, 2019'
+    },
+    {
+      companyName: 'Objective Solutions',
+      initialDate: 'August, 2019',
+      finalDate: 'Until today'
+    }
+  ]
+}, null, 2))
